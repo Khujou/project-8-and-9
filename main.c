b03be0ce1dcc632ea51b4d02a4e42821f13f088e
Thomas Svilpa <66971388+Khujou@users.noreply.github.com>
2022-11-22 00:18:45 +0000

    but this is the most recent!

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 void writeToFile(char *clubName, char *league, char *leagueRegion,
                  char *stadium, int leagueTitles, FILE *fp) {
 
-  Soccer *team = calloc(1, sizeof(Soccer));
+  Soccer *team = calloc(1, sizeof(Soccer) - sizeof(Soccer*));
 
   strcpy(team->club_name, clubName);
   strcpy(team->league, league);
@@ -14,7 +14,7 @@ void writeToFile(char *clubName, char *league, char *leagueRegion,
   strcpy(team->stadium, stadium);
   team->league_titles = leagueTitles;
 
-  fwrite(team, sizeof(Soccer), 1, fp);
+  fwrite(team, sizeof(Soccer) - sizeof(Soccer*), 1, fp);
 }
 
 void secret(char *begin, char *variable, char *end) {
@@ -25,44 +25,42 @@ void secret(char *begin, char *variable, char *end) {
   printf("%s", end);
 }
 
-void printOut(Soccer *teams[5]) {
+void printOut(Soccer *team) {
 
   char *str = malloc(3);
 
-  for (int i = 0; i < 5; i++) {
+  while (team->next != NULL) {
 
-    secret("\nTheir team name is ", teams[i]->club_name, ",\n");
-    secret("They play in the ", teams[i]->league, " League,\n");
-    secret("Their team is based in ", teams[i]->league_region, ",\n");
-    secret("They play at ", teams[i]->stadium, ",\n");
-    sprintf(str, "%d", teams[i]->league_titles);
+    secret("\nTheir team name is ", team->club_name, ",\n");
+    secret("They play in the ", team->league, " League,\n");
+    secret("Their team is based in ", team->league_region, ",\n");
+    secret("They play at ", team->stadium, ",\n");
+    sprintf(str, "%d", team->league_titles);
     secret("They have won ", str, " League Title(s).\n");
+
+    team = team->next;
   }
+
   printf("\n");
 }
 
 int main(void) {
 
   // INITIALIZING VARIABLES ----------------------------------------------------
-  Soccer *teams[5];
-  for (int i = 0; i < 5; i++)
-    teams[i] = calloc(1, sizeof(Soccer));
-
+  Soccer *team = calloc(1, sizeof(Soccer));
   long numBytesRead;
 
   // OPENING FILE FOR WRITING --------------------------------------------------
   FILE *fp;
 
-  /*
-  
   fp = fopen("soccer.dat", "wb");
-    
+
   if (fp == NULL) {
     printf("failed to open file\n");
     return 1;
   }
 
-  // WRITING STRUCTS TO FILE ---------------------------------------------------
+  // WRITING STRUCTS TO FILE---------------------------------------------------
   writeToFile("Sounders", "MLS", "Seattle", "Lumen Field", 2, fp);
   writeToFile("Galaxy", "MLS", "Los Angeles", "Dignity Health Sports Park", 5,
               fp);
@@ -73,8 +71,6 @@ int main(void) {
   // CLOSING FILE FOR WRITING --------------------------------------------------
   fclose(fp);
 
-  */
-
   // OPENING FILE FOR READING --------------------------------------------------
   fp = fopen("soccer.dat", "rb");
   if (fp == NULL) {
@@ -84,15 +80,17 @@ int main(void) {
 
   // OUTPUTTING STRUCT DATA THAT WAS READ IN -----------------------------------
 
-  int i = 0;
-  numBytesRead = fread(teams[i], sizeof(Soccer), 1, fp);
+  numBytesRead = fread(team, sizeof(Soccer) - sizeof(team), 1, fp);
+  Soccer *tail = team;
 
   while (numBytesRead != 0) {
-    i++;
-    numBytesRead = fread(teams[i], sizeof(Soccer), 1, fp);
+    Soccer *tmpTeam = calloc(1, sizeof(Soccer));
+    numBytesRead = fread(tmpTeam, sizeof(Soccer) - sizeof(team), 1, fp);
+    tail->next = tmpTeam;
+    tail = tmpTeam;
   }
 
-  printOut(teams);
+  printOut(team);
 
   // CLOSING FILE --------------------------------------------------------------
   fclose(fp);
